a11e3e36ee09de77ed15d6de3f4d1fa4e601f0ce
fosterion <32929118+fosterion@users.noreply.github.com>
2021-04-18 19:26:15 +0300

    Implement command.

--- src/CrystalFramework/Commands/CrystalCommand.cs
+++ src/CrystalFramework/Commands/CrystalCommand.cs
@@ -1,24 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace CrystalFramework.Commands
 {
     class CrystalCommand : ICommand
     {
-        public event EventHandler CanExecuteChanged;
+        private Action<object> _execute;
+        private Func<object, bool> _canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public CrystalCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
 
         public bool CanExecute(object parameter)
         {
-            throw new NotImplementedException();
+            return _canExecute == null || _canExecute(parameter);
         }
 
         public void Execute(object parameter)
         {
-            throw new NotImplementedException();
+            _execute(parameter);
         }
     }
 }
