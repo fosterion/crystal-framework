46e5ed1fc599d963c40a42dcad0505d1e3a68128
fosterion <32929118+fosterion@users.noreply.github.com>
2021-04-19 23:47:23 +0300

    Add extensions.

---
+++ src/CrystalFramework/Extensions/DataTableExtensions.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace CrystalFramework.Extensions
+{
+    public static class DataTableExtensions
+    {
+        /// <summary>
+        /// Convert DataTable to List
+        /// </summary>
+        /// <typeparam name="T">List datatype</typeparam>
+        /// <param name="source">Source table</param>
+        public static List<T> ToList<T>(this DataTable source) where T : new()
+        {
+            List<T> list = new List<T>();
+
+            var typeProperties = typeof(T).GetProperties().Select(propertyInfo => new
+            {
+                PropertyInfo = propertyInfo,
+                Type = Nullable.GetUnderlyingType(propertyInfo.PropertyType) ?? propertyInfo.PropertyType
+            }).ToList();
+
+            foreach (var row in source.Rows.Cast<DataRow>())
+            {
+                T obj = new T();
+
+                foreach (var typeProperty in typeProperties)
+                {
+                    object value = row[typeProperty.PropertyInfo.Name];
+
+                    object safeValue = value == null || DBNull.Value.Equals(value)
+                        ? null
+                        : Convert.ChangeType(value, typeProperty.Type);
+
+                    typeProperty.PropertyInfo.SetValue(obj, safeValue, null);
+                }
+
+                list.Add(obj);
+            }
+
+            return list;
+        }
+
+        /// <summary>
+        /// Gently rename column if it exists
+        /// </summary>
+        /// <param name="source">Таблица</param>
+        /// <param name="oldName">Current column name</param>
+        /// <param name="newName">New column name</param>
+        public static DataTable RenameColumn(this DataTable source, string oldName, string newName)
+        {
+            if (source.Columns.Contains(oldName))
+                source.Columns[oldName].ColumnName = newName;
+
+            return source;
+        }
+
+        /// <summary>
+        /// Gently remove column if it exists
+        /// </summary>
+        /// <param name="source">Source table</param>
+        /// <param name="columnName">Column name for removing</param>
+        public static DataTable RemoveColumn(this DataTable source, string columnName)
+        {
+            if (source.Columns.Contains(columnName))
+                source.Columns.Remove(columnName);
+
+            return source;
+        }
+    }
+}
---
+++ src/CrystalFramework/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrystalFramework.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        /// <summary>
+        /// Convert to table. Version 1
+        /// </summary>
+        /// <typeparam name="T">Datatype</typeparam>
+        /// <param name="source">Collection</param>
+        /// <returns></returns>
+        public static DataTable ToDataTable1<T>(this IList<T> source)
+        {
+            PropertyDescriptorCollection props = TypeDescriptor.GetProperties(typeof(T));
+
+            DataTable table = new DataTable();
+
+            for (int i = 0; i < props.Count; i++)
+            {
+                PropertyDescriptor prop = props[i];
+                table.Columns.Add(prop.Name, prop.PropertyType);
+            }
+
+            object[] values = new object[props.Count];
+
+            foreach (T item in source)
+            {
+                for (int i = 0; i < values.Length; i++)
+                {
+                    values[i] = props[i].GetValue(item);
+                }
+
+                table.Rows.Add(values);
+            }
+
+            return table;
+        }
+
+        /// <summary>
+        /// Convert to table. Version 2
+        /// </summary>
+        /// <typeparam name="T">Datatype</typeparam>
+        /// <param name="source">Collection</param>
+        /// <returns></returns>
+        public static DataTable ToDataTable2<T>(this IList<T> source)
+        {
+            PropertyDescriptorCollection properties = TypeDescriptor.GetProperties(typeof(T));
+
+            DataTable table = new DataTable();
+
+            foreach (PropertyDescriptor prop in properties)
+            {
+                table.Columns.Add(prop.Name, Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType);
+            }
+
+            foreach (T item in source)
+            {
+                DataRow row = table.NewRow();
+
+                foreach (PropertyDescriptor prop in properties)
+                {
+                    row[prop.Name] = prop.GetValue(item) ?? DBNull.Value;
+                }
+
+                table.Rows.Add(row);
+            }
+
+            return table;
+        }
+
+        /// <summary>
+        /// Unique values by the specified field
+        /// </summary>
+        /// <typeparam name="TItem">Datatype</typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="source">Source collection</param>
+        /// <param name="selector">Predicate</param>
+        /// <returns></returns>
+        public static IEnumerable<TItem> DistinctBy<TItem, TValue>(this IEnumerable<TItem> source, Func<TItem, TValue> selector)
+        {
+            var uniques = new HashSet<TValue>();
+
+            foreach (var item in source)
+            {
+                if (uniques.Add(selector(item))) yield return item;
+            }
+        }
+
+        /// <summary>
+        /// Convert to SQL-typed table
+        /// </summary>
+        /// <typeparam name="T">Datatype</typeparam>
+        /// <param name="source">Collection</param>
+        public static DataTable ToSqlTable<T>(this IEnumerable<T> source)
+        {
+            var table = new DataTable();
+            table.Columns.Add(new DataColumn("Value", typeof(T)));
+
+            foreach (T item in source)
+            {
+                var row = table.NewRow();
+                row.ItemArray = new object[] { item };
+                table.Rows.Add(row);
+            }
+
+            return table;
+        }
+
+        /// <summary>
+        /// Clone collection
+        /// </summary>
+        /// <typeparam name="T">Datatype</typeparam>
+        /// <param name="source">Collection</param>
+        /// <returns></returns>
+        public static IEnumerable<T> Clone<T>(this IEnumerable<T> source) where T : ICloneable
+        {
+            var list = source.Select(item => (T)item.Clone()).ToList();
+            var result = new List<T>();
+
+            foreach (var item in list)
+                result.Add(item);
+
+            return result;
+        }
+    }
+}
