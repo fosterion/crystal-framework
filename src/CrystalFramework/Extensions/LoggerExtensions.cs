0fb1de8dcbf365e0f4d029bd3ef4c4272d0e1428
fosterion <32929118+fosterion@users.noreply.github.com>
2021-05-22 15:53:22 +0300

    Add simple logger.

---
+++ src/CrystalFramework/Extensions/LoggerExtensions.cs
@@ -0,0 +1,27 @@
+﻿using CrystalFramework.Logger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrystalFramework.Extensions
+{
+    public static class LoggerExtensions
+    {
+        public static void LogInfo(this CrystalLogger logger, string message)
+        {
+            logger.Log(LogType.Info, message);
+        }
+
+        public static void LogWarning(this CrystalLogger logger, string message)
+        {
+            logger.Log(LogType.Warning, message);
+        }
+
+        public static void LogError(this CrystalLogger logger, string message)
+        {
+            logger.Log(LogType.Error, message);
+        }
+    }
+}
---
+++ src/CrystalFramework/Logger/CrystalLogger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrystalFramework.Logger
+{
+    public class CrystalLogger
+    {
+        internal void Log(LogType type, string message)
+        {
+            Log($"{nameof(type)}: {message}");
+        }
+
+        private void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
